65ad05216b68424cc2dc151fa0b084231b80c1aa
Máximo Liberata Torres <maximoliberata0@gmail.com>
2025-05-31 11:36:46 -0400

    feat: add course exercise 4.3

---
+++ Course/4_classes/4_3_Quiz_Practice Time/src/Main.kt
@@ -0,0 +1,7 @@
+package spices
+
+fun main () {
+    val simpleSpice = SimpleSpice()
+
+    println("-- SimpleSpice --\nName: ${simpleSpice.name}\nHeat: ${simpleSpice.heat}")
+}
---
+++ Course/4_classes/4_3_Quiz_Practice Time/src/spices/SimpleSpice.kt
@@ -0,0 +1,8 @@
+package spices
+
+class SimpleSpice {
+    var name: String = "curry"
+    var spiciness: String = "mild"
+    val heat: Int
+        get() { return 5 }
+}

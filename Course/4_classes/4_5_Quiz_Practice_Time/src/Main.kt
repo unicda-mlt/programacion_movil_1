b4b5df8ee48d434d2aaf66b7c1cc6d7d1996b5e9
Máximo Liberata Torres <maximoliberata0@gmail.com>
2025-05-31 12:12:04 -0400

    feat: add course exercise 4.5

---
+++ Course/4_classes/4_5_Quiz_Practice_Time/src/Main.kt
@@ -0,0 +1,23 @@
+package spice
+
+import kotlin.random.Random
+
+fun main () {
+    println("Portion of salt: ${getGramOfSalt()}gm\n")
+
+    val spices = listOf(
+        Spice("Bell Pepper", "mild"),
+        Spice("Poblano", "medium"),
+        Spice("Jalapeño", "hot"),
+        Spice("Serrano", "hot"),
+        Spice("Habanero", "extrahot")
+    )
+
+    println("\nList of sweet spices:")
+
+    for (spice in spices.filter { it.heat < 10000 }) {
+        println(spice.toString())
+    }
+}
+
+fun getGramOfSalt () = Random.nextInt(1, 451)
---
+++ Course/4_classes/4_5_Quiz_Practice_Time/src/spice/Spice.kt
@@ -0,0 +1,29 @@
+package spice
+
+import kotlin.random.Random
+
+class Spice (val name: String, val spiciness: String = "mild") {
+
+    init {
+        println(toString())
+    }
+
+    val heat: Int
+        get() { return calculateSpicinessHeat(spiciness) }
+
+    private fun calculateSpicinessHeat (spiciness: String): Int {
+        return when (spiciness.lowercase()) {
+            "mild" -> Random.nextInt(1000 + 1)
+            "medium" -> Random.nextInt(2500, 30001)
+            "hot" -> Random.nextInt(30001, 100001)
+            "extrahot" -> Random.nextInt(100001, 300001)
+            "extremely" -> Random.nextInt(300001, 10000000)
+            else -> 0
+        }
+    }
+
+    override fun toString(): String {
+        return "[$name]-[$spiciness]: $heat"
+    }
+
+}

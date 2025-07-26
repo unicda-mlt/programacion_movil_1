3102c48c9a14fb217359c8bcbbe5c13824135d7d
Máximo Liberata Torres <maximoliberata0@gmail.com>
2025-07-26 09:31:15 -0400

    feat: add all exercices of lab 2025-07-17

---
+++ Labs/AndroidStudio/AndroidProject/app/src/main/java/com/labs_2027_07_17/Exercise_4.kt
@@ -0,0 +1,103 @@
+package com.labs_2027_07_17
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.ui.theme.AppTheme
+
+
+@Composable
+fun Lab20250717_Exercise_4_Screen() {
+    AppDeSaludos()
+}
+
+@Composable
+fun AppDeSaludos() {
+    var nombre by remember { mutableStateOf("") }
+    var textNombre by remember { mutableStateOf("") }
+    var saludo by remember { mutableStateOf(false) }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(10.dp, 50.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            modifier = Modifier.fillMaxWidth(),
+            text = if (nombre.isEmpty() || !saludo) "" else "¡Hola, $textNombre!",
+            style = TextStyle(
+                color = MaterialTheme.colorScheme.secondary,
+                fontSize = 18.sp,
+                textAlign = TextAlign.Center
+            )
+        )
+
+        Spacer(Modifier.height(40.dp))
+
+        TextField(
+            modifier = Modifier.fillMaxWidth(),
+            value = nombre,
+            onValueChange = {
+                nombre = it
+            },
+            label = {
+                Text(text = "Nombre")
+            }
+        )
+
+        Spacer(Modifier.height(30.dp))
+
+        OutlinedButton (
+            modifier = Modifier.fillMaxWidth(0.8f),
+            colors = ButtonDefaults.buttonColors(
+                containerColor = MaterialTheme.colorScheme.surfaceContainerHighest
+            ),
+            onClick = {
+                saludo = true
+                textNombre = nombre
+            }
+        ) {
+            Text(
+                text = "Saludar",
+                style = TextStyle(
+                    fontSize = 30.sp,
+                    fontWeight = FontWeight.SemiBold,
+                    color = MaterialTheme.colorScheme.surfaceTint,
+                )
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun Lab20250717_Exercise_4_Screen_Preview() {
+    AppTheme (
+        darkTheme = false,
+        dynamicColor = false
+    ) {
+        Lab20250717_Exercise_4_Screen()
+    }
+}
--- Labs/AndroidStudio/AndroidProject/app/src/main/java/com/route/AppNavHost.kt
+++ Labs/AndroidStudio/AndroidProject/app/src/main/java/com/route/AppNavHost.kt
@@ -16,6 +16,16 @@ import com.labs_2027_07_08.Lab20250708_Exercise_7_Screen
 import com.labs_2027_07_08.Lab20250708_Exercise_8_Screen
 import com.labs_2027_07_08.Lab20250708_Exercise_9_Screen
 import com.labs_2027_07_17.Lab20250717Screen
+import com.labs_2027_07_17.Lab20250717_Exercise_10_Screen
+import com.labs_2027_07_17.Lab20250717_Exercise_1_Screen
+import com.labs_2027_07_17.Lab20250717_Exercise_2_Screen
+import com.labs_2027_07_17.Lab20250717_Exercise_3_Screen
+import com.labs_2027_07_17.Lab20250717_Exercise_4_Screen
+import com.labs_2027_07_17.Lab20250717_Exercise_5_Screen
+import com.labs_2027_07_17.Lab20250717_Exercise_6_Screen
+import com.labs_2027_07_17.Lab20250717_Exercise_7_Screen
+import com.labs_2027_07_17.Lab20250717_Exercise_8_Screen
+import com.labs_2027_07_17.Lab20250717_Exercise_9_Screen
 import com.main.androidproject.HomeScreen
 
 
@@ -64,6 +74,36 @@ fun AppNavHost(navController: NavHostController) {
             Lab20250717Screen(
                 router = ScreenRouter(Screen.Labs20250717, navController)
             )
+        }
+        composable(Screen.Labs20250717_Exercise_1.route) {
+            Lab20250717_Exercise_1_Screen()
+        }
+        composable(Screen.Labs20250717_Exercise_2.route) {
+            Lab20250717_Exercise_2_Screen()
+        }
+        composable(Screen.Labs20250717_Exercise_3.route) {
+            Lab20250717_Exercise_3_Screen()
+        }
+        composable(Screen.Labs20250717_Exercise_4.route) {
+            Lab20250717_Exercise_4_Screen()
+        }
+        composable(Screen.Labs20250717_Exercise_5.route) {
+            Lab20250717_Exercise_5_Screen()
+        }
+        composable(Screen.Labs20250717_Exercise_6.route) {
+            Lab20250717_Exercise_6_Screen()
+        }
+        composable(Screen.Labs20250717_Exercise_7.route) {
+            Lab20250717_Exercise_7_Screen()
+        }
+        composable(Screen.Labs20250717_Exercise_8.route) {
+            Lab20250717_Exercise_8_Screen()
+        }
+        composable(Screen.Labs20250717_Exercise_9.route) {
+            Lab20250717_Exercise_9_Screen()
+        }
+        composable(Screen.Labs20250717_Exercise_10.route) {
+            Lab20250717_Exercise_10_Screen()
         }
     }
 }
